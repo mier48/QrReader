d7220ed5df0eb01dc0ffef000281a856f4a37455
mier48 <100311364+mier48@users.noreply.github.com>
2022-05-02 15:31:20 +0200

    Basic configuration

---
+++ app/src/main/java/com/albertomier/qrreader/ui/view/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.albertomier.qrreader.ui.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.albertomier.qrreader.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
