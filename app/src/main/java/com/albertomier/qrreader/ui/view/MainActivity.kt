5eddf1dd3ca1bfbbd7a8ed7d70dfcb6b9745fcf6
mier48 <100311364+mier48@users.noreply.github.com>
2022-05-02 15:51:30 +0200

    add scanner

--- app/src/main/java/com/albertomier/qrreader/ui/view/MainActivity.kt
+++ app/src/main/java/com/albertomier/qrreader/ui/view/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.albertomier.qrreader.ui.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.albertomier.qrreader.databinding.ActivityMainBinding
+import com.google.zxing.integration.android.IntentIntegrator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,5 +17,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnScanner.setOnClickListener {
+            initScanner()
+        }
+    }
+
+    private fun initScanner() {
+        val integrator = IntentIntegrator(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        integrator.setPrompt("QR Scanner")
+        integrator.setTorchEnabled(false)
+        integrator.setBeepEnabled(true)
+        integrator.setOrientationLocked(true)
+        integrator.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Cancelado", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "El valor escaneado es: " + result.contents, Toast.LENGTH_LONG)
+                    .show()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
